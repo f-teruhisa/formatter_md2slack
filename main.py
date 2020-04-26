8bddfa2b30a1c38d71003ed09e904c1666bfb83c
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2020-04-26 17:36:45 +0900

    use txt instead of markdoen

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
-from bs4 import BeautifulSoup
-import markdown
 import os
 
-md = markdown.Markdown()
-
-def format_markdown_to_planetext(doc):
-    html = md.convert(doc)
-    text = ''.join(BeautifulSoup(html, features="html.parser").findAll(text=True))
+def format_textfile(txtfile):
+    text = txtfile.read()
     translate_checkbox_to_bullet(text)
 
 def translate_checkbox_to_bullet(text):
-    replaced_text = text.replace('[ ]', '-').replace('[x]', '-')
+    replaced_text = text.replace('[ ]', '').replace('[x]', '')
     output_txt_file(replaced_text)
 
 def output_txt_file(text):
-    with open('src/text.txt', mode='w') as t:
+    with open('src/formatted_text.txt', mode='w') as t:
         t.write(text)
+        print('Complete. Please check src/formatted_text.txt')
+        print('-----------------------------------------------')
+        print(text)
+        print('-----------------------------------------------')
 
-with open('src/document.md') as f:
-    md_document = f.read()
-    format_markdown_to_planetext(md_document)
+print('Start to convert...')
+f = open('src/text.txt')
+format_textfile(f)
+f.close()
