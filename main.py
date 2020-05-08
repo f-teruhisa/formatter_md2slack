2d0f981b674f98c3f1c1380faa0627904d5a74be
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2020-05-08 22:56:09 +0900

    update attachments

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 from os.path import join, dirname
 from dotenv import load_dotenv
 import requests
+import time
 
 def format_textfile(txtfile):
     """
@@ -16,7 +17,7 @@ def translate_checkbox_to_bullet(text):
     """
     Delete checkbox of markdown into bullet(-)
     """
-    replaced_text = text.replace('[ ]', '').replace('[x]', '')
+    replaced_text = text.replace('[ ]', '').replace('[x]', '').replace('[]', '')
     output_txt_file(replaced_text)
 
 def output_txt_file(text):
@@ -41,12 +42,21 @@ def send_slack_notification(formated_text):
     channel = os.environ.get("CHANNEL")
     user_name = os.environ.get("USER_NAME")
     user_mention = os.environ.get("USER_MENTION")
+    author_link = os.environ.get("USER_LINK")
+    author_icon = os.environ.get("USER_IMG_URL")
     text = f"今日の{user_name}のToDoです。よろしくお願いします。"
+    now_ts = time.time()
 
     attachments = [{
         'author_name': f"<@{user_mention}>",
+        'author_link': f"{author_link}",
+        'author_icon': f"{author_icon}",
+        'title': '本日のToDo',
         'text': formated_text,
-        'color': '#2eb886'
+        'color': '#2eb886',
+        'footer': f"Send from {user_mention}",
+        'footer_icon': f"{author_icon}",
+        'ts': str(now_ts)
     }]
 
     data = json.dumps({
